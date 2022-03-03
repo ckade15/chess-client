cdb9da284d4280c2c388251c1dfbe22f8a0dcced
Chris Kade <chriskade1999@gmail.com>
2022-03-03 02:52:00 -0500

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer(props) {
     
     if (props.type === 'mobile'){
         return (
-            <footer className="bottom-0 w-full bg-transparent bg-blue-100 p-3 flex flex-col place-items-center"> 
+            <footer className="bottom-0 fixed w-full bg-transparent bg-blue-100 p-3 flex flex-col place-items-center"> 
                 <div className="flex place-content-evenly mt-1 w-full">
                     <a href="https://www.linkedin.com/in/christopher-kade-b9b2151a5/" className="soc-link text-blue-400 not-italic font-bold text-2xl rounded-full h-10 w-10 text-center leading-8"><img src={linkedin}/></a>
                     <a href="https://twitter.com/Christhedev__" className="soc-link h-10 w-10 rounded-full flex justify-center"><img src={twitter} className="w-full" alt="twitter icon" /></a>
@@ -19,7 +19,7 @@ function Footer(props) {
       );
     }else{
         return (
-            <footer className="bottom-0 w-full bg-transparent bg-blue-100 p-10 flex justify-evenly text-xl">
+            <footer className="bottom-0 fixed w-full bg-transparent bg-blue-100 p-10 flex justify-evenly text-xl">
                 <div className="flex flex-col w-1/2 text-xl text-center">
                     <p className="text-xl text-blue-800 font-bold font-mono p-1 mt-2">&copy; Kade Web Solutions  {(new Date().getFullYear())}</p>
                     <p className="font-mono text-lg font-bold  mb-1">Made by Chris Kade</p>
